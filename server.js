94568e05b11754e6e7fa2eb41ca15ba891272b76
denzziljacob <denzziljacob@gmail.com>
2016-10-14 14:36:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,17 +64,12 @@ return htmlTemplate;}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/articleName',function (req, res){//articleName==article-one
+app.get('/articleName',function (req, res){
+    //articleName==article-one
 //articles(articleName)=={}content object for article-one
 var articleName = req.params.articleName;
-res.send(createTemplate(article-one));
+res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); });
-
-
-app.get('/article-three', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
