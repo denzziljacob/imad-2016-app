5976abae5a8a3bf6c3c14fec85e6ffc8b97fccd7
denzziljacob <denzziljacob@gmail.com>
2016-10-15 11:14:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ var articles={
  date:'October 10',
  content:`<p>
 This is the content for my first article.So far looks ok though eats a considerable time of mine.But Nevertheless lets see.
-</p>.`},
+</p>`.`<p>
+This is the content for my first article.So far looks ok though eats a considerable time of mine.But Nevertheless lets see.
+</p>.
 'article-two':{
 title:'Article Two/denzzil jacob',
 heading:'Article Two',
