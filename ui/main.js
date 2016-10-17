3f4df8881ba2dcc23335df39755faa6447475b1a
denzziljacob <denzziljacob@gmail.com>
2016-10-17 12:35:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,6 @@ var marginLeft= 0;
 function moveRight() { marginLeft=marginLeft+10;
 img.style.marginLeft=marginLeft+'px';}
 img.onclick=function(){
-var interval = setInterval(moveRight,100);
+var interval = setInterval(moveRight,50);
     
 };
