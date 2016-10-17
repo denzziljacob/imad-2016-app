5f9ba41084c8a3cd25875867bcfcdb155b5c1935
denzziljacob <denzziljacob@gmail.com>
2016-10-17 12:21:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element=document.getElementById('main-text');
 element.innerHTML= 'New Value';
 var img=document.getElementById('madi');
 img.onclick=function(){
-var interval = setInterval(moveLeft,100);
+var interval = setInterval(moveRight,100);
     
 };
