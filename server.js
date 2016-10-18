753addba6ddd282cfeb0135c744805142e1d4383
denzziljacob <denzziljacob@gmail.com>
2016-10-18 14:11:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,6 +59,10 @@ return htmlTemplate;}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter=0;
+
+app.get('/counter',function(req,res){ counter = counter+1;res.send(counter.toString());});
 app.get('/:articleName',function (req, res){
     //articleName==article-one
 //articles(articleName)=={}content object for article-one
@@ -76,6 +80,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
