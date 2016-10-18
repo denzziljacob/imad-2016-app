3d8b7f9d8dbe3bd12c24c23136f81c24b703a226
denzziljacob <denzziljacob@gmail.com>
2016-10-18 14:50:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,6 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 var img=document.getElementById('madi');
 var marginLeft= 0;
-
 function moveRight() { marginLeft=marginLeft+1;
 img.style.marginLeft=marginLeft+'px';}
 img.onclick=function(){
